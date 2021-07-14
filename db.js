451a516265f77270c1a1ce6d5d99ded3616fa4fa
jeff <jeffhinoto@gmail.com>
2021-07-14 05:24:15 -0300

    .

--- db.js
+++ db.js
@@ -1,7 +1,6 @@
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize({
-    dialect: 'sqlite',
-    storage: './database.sqlite'
+    dialect: 'mariadb'
 })
 
 module.exports = sequelize;
