523a88354eb48b113d966cf5cbf79dd00f95a6ca
jeff <jeffhinoto@gmail.com>
2021-07-14 05:36:13 -0300

    push

--- db.js
+++ db.js
@@ -1,6 +1,4 @@
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize({
-    dialect: 'mariadb'
-})
+const sequelize = new Sequelize(process.env.JAWSDB_MARIA_URL)
 
 module.exports = sequelize;
